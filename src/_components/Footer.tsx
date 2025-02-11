7625e43fa3cfb03abd81dff66cb09138f39b2a70
Adi Primanda Ginting <adi@adipginting.com>
2025-02-12 06:46:31 +0700

    update: update footer year

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="shadow-md">
-      <p>&copy; 2023 Adi Ginting. Bandung, Indonesia.</p>{" "}
+      <p>&copy; 2025 Adi Ginting. Bandung, Indonesia.</p>{" "}
     </footer>
   );
 };
