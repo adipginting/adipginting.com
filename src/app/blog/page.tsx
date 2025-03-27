dbe743ffaf7306e87719d0306a6beafaaed77de3
Adi Primanda Ginting <adi@adipginting.com>
2025-03-27 13:05:32 +0700

    fix

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main className="p-4 lg:justify-center lg:flex">
-    
       <p className="pt-4">Blog posts will come soon.</p>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,20 @@ export default function Home() {
         <h1 className="pt-4 pl-4 text-lg ">Hi, I am Adi Ginting. </h1>
         <p className="pt-1 pl-4 text-md">
           A computer programmer currently residing in West Java, Indonesia. I
-          have been focusing myself into web development in JavaScript ecosystem since
-          2021 and I am striving to delve more.
+          have been focusing myself into web development in JavaScript ecosystem
+          since 2021 and I am striving to delve more.
         </p>
 
         <p className="pt-1 pl-4 text-md">
-          The current technologies that I use are TypeScript, NodeJS (Express and NestJS), Reactjs (NextJS &
-          React Router 7), PostgreSQL, Linux (Debian & Ubuntu). On the side, I
-          also learn about Python, Golang, and PHP (Laravel).
+          The current technologies that I use are TypeScript, NodeJS (Express
+          and NestJS), Reactjs (NextJS & React Router 7), PostgreSQL, Linux
+          (Debian & Ubuntu). On the side, I also learn about Python, Golang, and
+          PHP (Laravel).
         </p>
         <p className="pt-1 pl-4 text-md">
-         I am a fan (a new convert) of good software engineering practices (Clean Code and Clean Architecture) and I strive to apply them whenever it is possible.
+          I am a fan (a new convert) of good software engineering practices
+          (Clean Code and Clean Architecture) and I strive to apply them
+          whenever it is possible.
         </p>
       </div>
     </main>
