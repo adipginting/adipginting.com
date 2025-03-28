9f9e4db49e14cea75f7e76457f620d5057deec54
Adi Primanda Ginting <adi@adipginting.com>
2025-03-28 19:17:43 +0700

    prettier on footer

--- src/_components/Footer.tsx
+++ src/_components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
     <footer className="flex items-center justify-center text-xs  h-[40px]">
-
-      <div className="shadow-md bg-slate-100">&copy; 2025 Adi Ginting. Bandung, Indonesia.</div>
+      <div className="shadow-md bg-slate-100">
+        &copy; 2025 Adi Ginting. Bandung, Indonesia.
+      </div>
     </footer>
   );
 };
