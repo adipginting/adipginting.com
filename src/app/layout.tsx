d6be40977d933dc99da2c8e17eacb8a81cc4583a
Adi Primanda Ginting <adi@adipginting.com>
2025-05-19 08:27:14 +0700

    prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import "./globals.css";
 import Navbar from "../_components/Navbar";
 import Footer from "../_components/Footer";
-import { Lato } from "next/font/google";
+import { Lato, Roboto } from "next/font/google";
 import cn from "classnames";
 import Hamburger from "@/_components/Hamburger";
 
-const notosans = Lato({ weight: ["300"], subsets: ["latin"] });
+const roboto = Roboto({ weight: ["300"], subsets: ["latin"] });
 
 export const metadata = {
   title: "Adi Ginting",
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head></head>
-      <body className={cn(notosans.className)}>
+      <body className={cn(roboto.className)}>
         <div>
           <Navbar />
           <Hamburger />
